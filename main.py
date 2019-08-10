c31d34d338f1d0e357bf3c036702c6c17163b379
fenshitianyue <abzanda@163.com>
2019-08-11 01:42:29 +0800

    update code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
-# import json
+import json
 import MySQLdb
 
 app = Flask(__name__)
@@ -68,18 +68,20 @@ def user_register():
         3.根据插入成功与否，给前端ajax返回相应的对象内容
     '''
     # 从前端获取数据
-    username = request.values.get('username') # 用户名 (string)
-    password = request.values.get('password') # 用户密码 (string)
-    workStatus = request.values.get('workStatus') # 工作状态 (int)
-    realName = request.values.get('realName') # 真实姓名 (string)
-    school = request.values.get('school') # 学校名 (string)
-    company = request.values.get('company') # 公司名 (string)
-    QQId = request.values.get('QQId') # 通讯软件账号 (string)
-    research = request.values.get('research') # 研究方向 (string)
-    education = request.values.get('education') # 教育水平 (string)
-    phone = request.values.get('phone') # 手机号 (string)
-    inviteCode = request.values.get('inviteCode') # 邀请码 (string)
-    # nation = request.values.get('nation') # 手机号所属区域 (int)
+    data = request.get_json()
+    data = json.loads(request.get_data())
+    username = data['username']
+    password = data['password'] # 用户密码 (string)
+    workStatus = data['workStatus'] # 工作状态 (int)
+    realName = data['realName'] # 真实姓名 (string) 编码是unocode,查看需要decode
+    school = data['school'] # 学校名 (string) 编码是unocode,查看需要decode
+    company = data['company'] # 公司名 (string) 编码是unocode,查看需要decode
+    QQId = data['QQId'] # 通讯软件账号 (string)
+    research = data['research'] # 研究方向 (string) 编码是unocode,查看需要decode
+    education = data['education'] # 教育水平 (string) 编码是unocode,查看需要decode
+    phone = data['phone'] # 手机号 (string)
+    inviteCode = data['inviteCode'] # 邀请码 (string)
+    # nation = data['nation'] # 手机号所属区域 (int)
 
     # # 连接MySQL数据库并获取到数据库句柄
     # db = MySQLdb.connect("localhost", "root", "nihao.", "itkim", charset='utf8')
