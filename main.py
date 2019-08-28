600724fa0adcb7707635976dffe94633538be2ec
fenshitianyue <abzanda@163.com>
2019-08-28 15:10:26 +0800

    complete the query order module

--- main.py
+++ main.py
@@ -259,23 +259,6 @@ def assignList():
         3.将查找到的数据拼接成json格式发送给前端
     '''
     path_pre = "/download/"
-    # dict = {}
-    # dict['id'] = 4000
-    # dict['title'] = "基于Unix的微型操作系统"
-    # dict['orderTag'] = "C"
-    # dict['publishTime'] = "20180810000000"
-    # dict['dueTime'] = "20190820000000"
-    # dict['requireType'] = "操作系统"
-    # dict['devPrice'] = "10000"
-    # dict['orderStatus'] = "辅导中"
-    # dict['docFilePath'] = path_pre + "document/doc/test.doc"
-    # dict['allFilePath'] = path_pre + "attachment/interface.zip"
-    # dict['requirement'] = "详细需求请联系(qq)：1262167092~"
-    # dict['devRemark'] = "请勿抄袭网上代码！"
-    # result = [dict]
-    # data是一个列表，列表元素类型是字典
-    # return jsonify(total=1, data=result)
-
     # 从前端获取分页相关信息
     data = request.get_json()
     data = json.loads(request.get_data())
@@ -285,10 +268,34 @@ def assignList():
     db = MySQLdb.connect("localhost", "root", "nihao.", "itkim", charset='utf8')
     cursor = db.cursor()
     # 拼接查询SQL
-    sql = "select * from dev limit " + str(int(pageNumber) * int(pageSize)) + "," + pageSize
+    sql = "select * from orders limit " + str((pageNumber-1) * pageSize) + "," + str(pageSize)
     cursor.execute(sql)
-    # TODO:根据查询结果拼接响应数据格式
-
+    # 根据查询结果拼接响应数据格式
+    results = cursor.fetchall()
+    if not results:
+        return jsonify(total=0, data=[])
+    num = 0
+    response = []
+    for row in results:
+        dict = {}
+        dict['id'] = row[0]
+        dict['title'] = row[1]
+        # 对时间戳做一个处理
+        dict['publishTime'] = row[2]
+        dict['dueTime'] = row[3]
+        dict['orderTag'] = row[4]
+        dict['requireType'] = row[5]
+        dict['devPrice'] = row[6]
+        dict['docFilePath'] = path_pre + row[7]
+        dict['allFilePath'] = path_pre + row[8]
+        dict['requirement'] = row[9]
+        dict['devRemark'] = row[10]
+        dict['orderStatus'] = row[11]
+        dict['pickFlag'] = row[12]
+        response.append(dict)
+        num = num + 1
+    db.close()
+    return jsonify(total=num, data=response)
 
 # 接订单
 @app.route('/orders/pickOrder')
