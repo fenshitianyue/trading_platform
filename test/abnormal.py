36d52006e45a68fc65d20d28284eb1f162f33659
fenshitianyue <abzanda@163.com>
2019-10-20 17:11:18 +0800

    update code

--- test/abnormal.py
+++ test/abnormal.py
@@ -1,21 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding:UTF-8 -*-
 
-def get_string(status):
-    if 1 == status:
-        return "string 1"
-    elif 2 == status:
-        return "string 2"
-    else:
-        raise Exception("none faild!")
+# def get_string(status):
+#     if 1 == status:
+#         return "string 1"
+#     elif 2 == status:
+#         return "string 2"
+#     else:
+#         raise Exception("none faild!")
+#
+# def func_top():
+#     try:
+#         string = get_string(3)
+#     except Exception, err:
+#         print err
+#         return
+#     print string
 
-def func_top():
+def test_file():
     try:
-        string = get_string(3)
-    except Exception, err:
-        print err
-        return
-    print string
+        fd = open("testfile", "w")
+        try:
+            fd.write("this is a test file, useing to test abnormal!!!")
+        finally:
+            print "close file!"
+            fd.close()
+    except IOError:
+        print "Error: file not found or read file faild!"
 
 if __name__ == '__main__':
-    func_top()
+    # func_top()
+    test_file()
